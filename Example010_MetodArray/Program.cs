ad08997dedc264e3d644e7f36a9bc3aab1ffac25
AlexeySmirnov93 <voinvetra92@mail.ru>
2022-10-18 16:39:49 +0300

    добавили программу, функции поиска индекса в массиве

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12,23,30,94,84,75,46,37,84};
+
+int n = array.Length;
+int find = 84;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
